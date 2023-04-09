ad3cc2a22352f6cd8604d5c272517ae9a6a9679c
Olexandr-Sliusar <103962458+Olexandr-Sliusar@users.noreply.github.com>
2023-04-09 21:54:35 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { GlobalStyle } from './GlobalStyle';
 import { Component } from 'react';
+
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
